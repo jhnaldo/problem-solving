2396b15ac1c5f349f3d3e497781eb8688c7d1a8a
jhnaldo <jhpjhp0223@gmail.com>
2021-06-10 01:53:04 +0900

    [ACMICPC] Solved 1167

---
+++ acmicpc/1167.cc
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <list>
+#include <queue>
+
+using namespace std;
+
+#define SIZE 100000
+
+class Edge {
+public:
+  int to, weight;
+  Edge(int to, int weight): to(to), weight(weight) {}
+};
+
+int n, max_dist, X, Y;
+list<Edge*> edges[SIZE];
+int dist[SIZE];
+
+int main() {
+  scanf("%d", &n);
+  for (int i = 0; i < n; i++) {
+    int x, y, w;
+    scanf("%d", &x);
+    x--;
+    while (true) {
+      scanf("%d", &y);
+      if (y == -1) break;
+      y--;
+      scanf("%d", &w);
+      edges[x].push_back(new Edge(y, w));
+    }
+  }
+
+  queue<int> q;
+  list<Edge*>::iterator iter;
+
+  for (int i = 0; i < n; i++) dist[i] = -1;
+  dist[0] = 0;
+  q.push(0);
+  max_dist = 0;
+  X = 0;
+  while(!q.empty()) {
+    int cur = q.front();
+    int d = dist[cur];
+    list<Edge*> list = edges[cur];
+    q.pop();
+    for (iter = list.begin(); iter != list.end(); iter++) {
+      Edge* edge = *iter;
+      int to = edge->to;
+      int weight = edge->weight;
+      if (dist[to] == -1) {
+        dist[to] = d + weight;
+        q.push(to);
+        if (max_dist < dist[to]) {
+          max_dist = dist[to];
+          X = to;
+        }
+      }
+    }
+  }
+
+  for (int i = 0; i < n; i++) dist[i] = -1;
+  dist[X] = 0;
+  q.push(X);
+  max_dist = 0;
+  Y = X;
+  while(!q.empty()) {
+    int cur = q.front();
+    int d = dist[cur];
+    list<Edge*> list = edges[cur];
+    q.pop();
+    for (iter = list.begin(); iter != list.end(); iter++) {
+      Edge* edge = *iter;
+      int to = edge->to;
+      int weight = edge->weight;
+      if (dist[to] == -1) {
+        dist[to] = d + weight;
+        q.push(to);
+        if (max_dist < dist[to]) {
+          max_dist = dist[to];
+          Y = to;
+        }
+      }
+    }
+  }
+  printf("%d\n", max_dist);
+
+  return 0;
+}
